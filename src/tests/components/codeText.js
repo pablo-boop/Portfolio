45a3b44664150011b86d6bfc633aa6f8dd6b0b5f
pablo-boop <pablocouto.senai564@gmail.com>
2023-09-04 15:22:02 -0300

    feat> finalization hero section from main

---
+++ src/tests/components/codeText.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './css/codeText.css'
+
+function CodeTxt() {
+    return (
+        <div class="wrapper">
+            <p class="static-txt">Programador de</p>
+            <ul class="dynamic-txts">
+                <li><span></span></li>
+                <li><span>JavaScript</span></li>
+                <li><span>React</span></li>
+                <li><span>NodeJS</span></li>
+            </ul>
+        </div>
+    )
+}
+
+export default CodeTxt
--- src/tests/index.js
+++ src/tests/index.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Fragment } from "react";
 import './style.css';
 import NavBar from './components/navBar';
 import Image from "./components/svgs";
+import CodeTxt from "./components/codeText";
 
 const Main = () => {
     return(
         <main>
             <NavBar/>
             <div id="hero">
+                <div id="txt-container">
                 <h1>Olá, eu sou Pablo Daniel Couto</h1>
+                <CodeTxt/>
+                </div>
                 <Image/>
             </div>
         </main>
